d5022ebbaa16437742eaa4ca6afde516eea8e3f4
atharv4git <atharv4study@gmail.com>
2022-12-04 10:10:48 +0530

    bunch of files created

--- sensor/config.py
+++ sensor/config.py
@@ -1,14 +1,14 @@
 import pymongo
 import pandas as pd
-import json
 from dataclasses import dataclass
+# Provide the mongodb localhost url to connect python to mongodb.
 import os
 
 @dataclass
 class EnvironmentVariable:
-    mongo_db_url:str = os.getenv("MONGO_DB_URL")
-    aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
-    aws_access_secret_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
+    mongo_db_url = os.getenv("MONGO_DB_URL")
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_access_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 
 env_var = EnvironmentVariable()
